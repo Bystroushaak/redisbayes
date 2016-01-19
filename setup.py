42f4fb16f92797340bd15cc72f7f8b530fc20b78
Bystroushaak <bystrousak@kitakitsune.org>
2016-01-19 17:11:33 +0100

    setup.py rewritten.

--- setup.py
+++ setup.py
@@ -1,36 +1,63 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
-# this is not a love song
-# http://packages.python.org/distribute/setuptools.html
-# http://diveintopython3.org/packaging.html
-# http://wiki.python.org/moin/CheeseShopTutorial
-# http://pypi.python.org/pypi?:action=list_classifiers
-
-from ez_setup import use_setuptools
-use_setuptools(version="0.6c11")
-
-import os
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
 from setuptools import setup
+from setuptools import find_packages
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+from docs import getVersion
 
+
+# Variables ===================================================================
+changelog = open('CHANGELOG.rst').read()
+long_description = "\n\n".join([
+    open('README.rst').read(),
+    changelog
+])
+
+
+# Actual setup definition =====================================================
 setup(
-    name                 = "redisbayes",
-    version              = __import__("redisbayes").__version__,
-    description          = u"Naïve Bayesian Text Classifier on Redis",
-    long_description     = read("README.md"),
-    author               = "Justine Tunney",
-    author_email         = "jtunney@gmail.com",
-    url                  = "https://github.com/jart/redisbayes",
-    license              = "MIT",
-    # install_requires     = [""],
-    py_modules           = ["redisbayes", "ez_setup"],
-    classifiers = [
-        "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: MIT License",
-        "Intended Audience :: Developers",
+    name='',
+    version=getVersion(changelog),
+    description="Naïve bayesian text classifier for any dict-like backend.",
+    long_description=long_description,
+    url='https://github.com/Bystroushaak/redisbayes',
+
+    author='Bystroushaak',
+    author_email='bystrousak@kitakitsune.org',
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        'Intended Audience :: Developers',
+
         "Programming Language :: Python",
-        "Topic :: Database",
-        "Topic :: Communications :: Email",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Topic :: Communications",
     ],
+    license='MIT',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+
+    zip_safe=False,
+    include_package_data=True,
+
+    test_suite='py.test',
+    tests_require=["pytest"],
+    extras_require={
+        "test": [
+            "pytest",
+        ],
+        "docs": [
+            "sphinx",
+            "sphinxcontrib-napoleon",
+        ]
+    },
 )
